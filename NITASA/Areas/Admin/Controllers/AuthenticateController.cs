7d34a57c5fc3a12d1aa2e5a6c04e9e1fb2d8b2e4
nikhil <nikhil.prajapati@drcsystems.com>
2015-06-19 15:58:23 +0530

    First

---
+++ NITASA/Areas/Admin/Controllers/AuthenticateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NITASA.Areas.Admin.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        //
+        // GET: /Admin/Authenticate/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
