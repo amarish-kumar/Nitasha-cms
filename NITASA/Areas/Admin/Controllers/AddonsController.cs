dee6b473c42e8b7ba8d504fbce5ffcd676431336
nikhil <nikhil.prajapati@drcsystems.com>
2015-06-23 12:26:24 +0530

    ActivityLogInitial

--- NITASA/Areas/Admin/Controllers/AddonsController.cs
+++ NITASA/Areas/Admin/Controllers/AddonsController.cs
@@ -1,4 +1,5 @@
 ﻿using NITASA.Data;
+using NITASA.Areas.Admin.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +17,28 @@ namespace NITASA.Areas.Admin.Controllers
         }
 
         public ActionResult List()
-        {
-            return View();
+        {          
+            bool ViewAllAddonsRights = UserRights.HasRights(Rights.ViewAllAddons);
+            bool ViewUnPublishedAddonsRights = UserRights.HasRights(Rights.ViewUnPublishedAddons);
+
+            if (!ViewAllAddonsRights && !ViewUnPublishedAddonsRights)
+                return RedirectToAction("AccessDenied", "Home");
+
+            //List<Category> categoryList = context.Category.Where(m => m.IsDeleted == false).ToList();
+            //ViewBag.categoryList = new SelectList(categoryList, "CategoryID", "CategoryName");
+
+            IQueryable<Content> Content = context.Content.Include("user");
+            if (ViewAllAddonsRights)
+                Content = Content.Where(m => m.Type.ToLower() != "post" && m.Type.ToLower() != "page" && m.IsDeleted == false);
+            else if (ViewUnPublishedAddonsRights)
+                Content = Content.Where(m => m.Type.ToLower() == "post" && m.Type.ToLower() != "page" && m.IsDeleted == false && m.isPublished == false);
+
+            //if (cid > 0)
+            //    Content = Content.Where(cont => cont.contentCategory.Where(m => m.CategoryID == cid).Count() > 0);
+
+            List<Content> ContentList = Content.OrderByDescending(m => m.PublishedOn).ToList();
+
+            return View(ContentList);
         }
         public ActionResult Add()
         {
@@ -29,6 +50,7 @@ namespace NITASA.Areas.Admin.Controllers
         {
             return View();
         }
+
         [HttpGet]
         public ActionResult Delete(string GUID)
         {
---
+++ NITASA/Areas/Admin/Helper/ActivityOperation.cs
@@ -0,0 +1,66 @@
+﻿using NITASA.Data;
+using NITASA.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NITASA.Areas.Admin.Helper
+{
+    public class ActivityOperation
+    {
+        public enum LogOperation
+        {
+            Add,
+            Update,
+            Delete,
+            Search,
+            View,
+            Active,
+            InActive,
+            Publish,
+            UnPublish
+        }
+
+        public static void InsertLog(LogOperation logOp, string title, string description, string pageUrl)
+        {
+            NTSDBContext db = new NTSDBContext();
+            int UserID = Common.CurrentUserID();
+            string operation = getOperationType(logOp);
+
+            ActivityLog log = new ActivityLog();
+            log.Title = title;
+            log.Description = description;
+            log.UserID = UserID;
+            log.AddedOn = DateTime.Now;
+            db.ActivityLog.Add(log);
+            db.SaveChanges();
+        }
+
+        public static string getOperationType(LogOperation logOp)
+        {
+            if (logOp == LogOperation.Add)
+                return "Add";
+            else if (logOp == LogOperation.Update)
+                return "Update";
+            else if (logOp == LogOperation.Delete)
+                return "Delete";
+            else if (logOp == LogOperation.Search)
+                return "Search";
+            else if (logOp == LogOperation.View)
+                return "View";
+            else if (logOp == LogOperation.Active)
+                return "Active";
+            else if (logOp == LogOperation.InActive)
+                return "InActive";
+            else if (logOp == LogOperation.Publish)
+                return "Publish";
+            else if (logOp == LogOperation.UnPublish)
+                return "UnPublish";
+
+
+            else
+                return "";
+        }
+    }
+}
