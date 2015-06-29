91bdaa9dcfb09c8a65c2f984e18b5b43938ea9be
nikhil <nikhil.prajapati@drcsystems.com>
2015-06-29 19:40:35 +0530

    addon layout change

--- NITASA/Controllers/CategoryController.cs
+++ NITASA/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NITASA.Data;
+using NITASA.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +8,18 @@ using System.Web.Mvc;
 
 namespace NITASA.Controllers
 {
+    [AllAction]
     public class CategoryController : Controller
     {
-        //
+        public NTSDBContext context;
+        public CategoryController()
+        {
+            this.context = new NTSDBContext();
+        }
+
         // GET: /Category/
         public ActionResult Details()
-        {
-            
+        {            
             return View();
         }
 	}
