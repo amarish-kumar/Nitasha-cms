5f974c9446213f81376c2acb8e74ac5031408101
sukhdev.zala <sukhdev.zala@drcsystems.com>
2015-07-09 11:29:03 +0530

    Implemented UserRoleCacheManager

---
+++ NITASA.Core/Caching/ICacheManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NITASA.Core.Caching
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+
+        T Set<T>(string key, object data, int cacheTime);
+
+        bool IsSet(string key);
+
+        void Remove(string key);
+
+        void Clear();
+    }
+}
