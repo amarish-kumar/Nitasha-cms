9265ea8072285bbc6605403e27e46803cd7a3792
nikhil <nikhil.prajapati@drcsystems.com>
2015-07-10 16:41:50 +0530

    QA Issues Resolved

--- NITASA.Data/User.cs
+++ NITASA.Data/User.cs
@@ -17,29 +17,29 @@ namespace NITASA.Data
         public string GUID { get; set; }
 
         [MaxLength(100)]
-        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
         [Required(ErrorMessage = "Please enter email address.")]
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email address.")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [MaxLength(100)]
-        [Required(ErrorMessage = "Please enter password")]
-        [StringLength(100, ErrorMessage = "Password must be atleast 8 characters long", MinimumLength = 8)]
+        [Required(ErrorMessage = "Please enter password.")]
+        [StringLength(100, ErrorMessage = "Password must be atleast 8 characters long.", MinimumLength = 8)]
         [RegularExpression("^(?=.*?[A-Z])(?=.*?[0-9]).{8,}$", ErrorMessage = "Password must contain at least one digit and one upper case letter and must be 8 characters long")]
         [Display(Name = "Password")]
         [AllowHtml]
         public string Password { get; set; }
 
         [MaxLength(50)]
-        [Required(ErrorMessage = "Please enter first name")]
-        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
+        [Required(ErrorMessage = "Please enter first name.")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
         [Display(Name = "First Name")]
         [AllowHtml]
         public string FirstName { get; set; }
 
         [MaxLength(50)]
-        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
         [Display(Name = "Last Name")]
         [AllowHtml]
         public string LastName { get; set; }
