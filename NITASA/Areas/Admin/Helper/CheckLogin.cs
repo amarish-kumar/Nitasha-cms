3b74f652594ebeb74e7011208b3d3f982c79cdaf
saraju <saraju.kabariya@drcsystems.com>
2015-07-10 15:12:40 +0530

    action filter debug

--- NITASA/Areas/Admin/Helper/CheckLogin.cs
+++ NITASA/Areas/Admin/Helper/CheckLogin.cs
@@ -4,11 +4,14 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NITASA.Services.Security;
 
 namespace NITASA.Areas.Admin.Helper
 {
     public class CheckLoginAttribute : ActionFilterAttribute
     {
+        IAclService aclService;
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             if (ConfigurationManager.AppSettings["Installed"] == null)
@@ -19,16 +22,17 @@ namespace NITASA.Areas.Admin.Helper
             {
                 HttpContext hContext = HttpContext.Current;
 #if DEBUG
-                if (hContext.Session["UserID"] == null) 
-                { 
+                if (hContext.Session["UserID"] == null)
+                {
                     hContext.Session["UserID"] = 1;
-                    hContext.Session["UserRole"] = new NITASA.Data.NTSDBContext().Role.Where(model => model.ID == 1 && model.IsDeleted == false).Select(m => m.Name).FirstOrDefault();
-                    UserRights.BindRights();
+                    var temp = new NITASA.Data.NTSDBContext().Role.Where(model => model.ID == 1 && model.IsDeleted == false);
+                    hContext.Session["UserRole"] = temp.Select(m => m.Name).FirstOrDefault();
+                    aclService.SetRights(1, temp.FirstOrDefault().ID);
                 }
 #endif
 
                 string url = HttpContext.Current.Request.Url.AbsoluteUri;
-                
+
                 if (hContext.Session["UserID"] == null)
                     filterContext.Result = new RedirectResult(@"~/Admin/Authenticate/Login?retUrl=" + HttpUtility.UrlEncode(url));
             }
