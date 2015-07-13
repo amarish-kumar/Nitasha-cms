caeebaf09ec51a330da0775c0177023e9f9ffa29
nikhil <nikhil.prajapati@drcsystems.com>
2015-07-13 17:44:28 +0530

    Final touch to VirtualConnect

--- NITASA/Controllers/AppController.cs
+++ NITASA/Controllers/AppController.cs
@@ -12,8 +12,8 @@ namespace NITASA.Controllers
         
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            //activeTheme = "/Views/themes/" + Request.RequestContext.HttpContext.Application["CurrentTheme"].ToString() + "/";
-            activeTheme = "/Views/themes/VirtualConnect/";
+            activeTheme = "/Views/themes/" + Request.RequestContext.HttpContext.Application["CurrentTheme"].ToString() + "/";
+            //activeTheme = "/Views/themes/VirtualConnect/";
             ViewBag.ActiveTheme = activeTheme;
             base.OnActionExecuting(filterContext);
         }
